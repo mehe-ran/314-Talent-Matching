701da0aedf6dafa041e7efbefa46e6e9eb83d668
Aakarshan Mani Giri <153081239+AakarshanManiGiri@users.noreply.github.com>
2026-04-29 13:22:11 +1000

    design and route candidate dashboard view

--- app.py
+++ app.py
@@ -73,5 +73,10 @@ def register():
 	return render_template('register.html')
 
 
+@app.route('/candidate')
+def candidate_dashboard():
+	return render_template('candidate_dashboard.html')
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
